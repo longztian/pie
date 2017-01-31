f3260e8cf01a2e8d441a85491d93c255fa8f9b15
Long <15257546+lzztt@users.noreply.github.com>
2017-01-30 21:51:50 -0600

    update upload middleware

--- formidable.js
+++ formidable.js
@@ -1,9 +1,53 @@
 import {
-  IncomingForm
+  IncomingForm,
 } from 'formidable'
 import fs from 'fs'
 import sizeOf from 'image-size'
+import sharp from 'sharp'
 import config from './config/image'
+
+const resize = (path, maxWidth, maxHeight) => new Promise((resolve, reject) => {
+  const newPath = `${config.dir}${path}`
+
+  sizeOf(path, (err, dimensions) => {
+    if (err) {
+      reject(err)
+    } else {
+      let factor = -1
+
+      if (dimensions.width > maxWidth) {
+        factor = dimensions.width / maxWidth
+      }
+
+      if (dimensions.height > maxHeight) {
+        factor = Math.max(dimensions.height / maxHeight, factor)
+      }
+
+      if (factor > 0) {
+        const width = Math.round(dimensions.width / factor)
+        const height = Math.round(dimensions.height / factor)
+        sharp(path)
+          .resize(width, height)
+          .toFile(newPath, (error) => {
+            if (error) {
+              reject(error)
+            } else {
+              // delete path
+              resolve(newPath)
+            }
+          })
+      } else {
+        fs.rename(path, newPath, (error) => {
+          if (error) {
+            reject(error)
+          } else {
+            resolve(newPath)
+          }
+        })
+      }
+    }
+  })
+})
 
 export default opts => (ctx, next) => {
   if (ctx.is('multipart')) {
@@ -15,18 +59,13 @@ export default opts => (ctx, next) => {
       // formidable should limit the body size and stop processing the req stream
       // not form middleware
       if (bytesReceived === 0) {
-        if (bytesExpected > ONE_HUNDRED_MB)
-          form._error(new Error('request body is too big'))
-      } else {
-        if (bytesReceived > bytesExpected)
-          form._error(new Error('got more data than expected'))
-      }
+        if (bytesExpected > config.totalLimit) { form._error(new Error('request body is too big')) }
+      } else if (bytesReceived > bytesExpected) { form._error(new Error('got more data than expected')) }
     })
 
     form.onPart = (part) => {
       if (part.filename) {
-        if (!config.types.includes(part.mime))
-          form._error(new Error(`unsupported type: ${part.mime}`))
+        if (!config.types.includes(part.mime)) { form._error(new Error(`unsupported type: ${part.mime}`)) }
       }
 
       form.handlePart(part)
@@ -46,16 +85,15 @@ export default opts => (ctx, next) => {
           }
         })
         .on('file', (name, file) => {
-          // check size
-          sizeOf(file.path, (err, dimensions) => {})
-          // rename temp file
-          fs.rename()
-          if (files[name]) {
-            if (!Array.isArray(files[name])) files[name] = [files[name]]
-            files[name].push(file)
-          } else {
-            files[name] = file
-          }
+          resize(file.path, config.width, config.height)
+            .then(() => {
+              if (files[name]) {
+                if (!Array.isArray(files[name])) files[name] = [files[name]]
+                files[name].push(file)
+              } else {
+                files[name] = file
+              }
+            })
         })
         .on('end', () => {
           ctx.request.body = {
